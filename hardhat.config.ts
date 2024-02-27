6edfc1c0cbe7c60cebca97647c5bb02fece003e2
fabian <fabian@MacBook-Air-de-fabian.local>
2024-02-27 16:15:04 -0300

    update configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-ethers";
-import * as dotenv from "dotenv";
 
-dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.20",
-      },
-      {
         version: "0.8.19"
       }
     ]
-  },  
+  },
   networks: {
-    goerli: {
-      url: "https://goerli.infura.io/v3/34a2351fd1f941e2b9f06fcb04438029",
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
       accounts: process.env.PRIVATE_KEY1 ? [process.env.PRIVATE_KEY1] : [],
     },
   },
 };
 
 export default config;
+
