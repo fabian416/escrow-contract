dc0aed3ce365a7d36f1f23d91365e68add3a16fb
fabian416 <diazfabian99@icloud.com>
2024-04-20 11:36:43 -0300

    remove tsconfig.json, add groupCreation script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,19 @@
 import { ethers } from "hardhat";
 
+
 async function main() {
 
   const usdcTokenAddress = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F";
+  const usdtTokenAddress = "";
+  const daiTokenAddress = "";
 
   //  get the signer (wallet) that is deploying the contract
   const [deployer] = await ethers.getSigners();
 
   //  print the deployer address and his balance
   console.log("Deploying contracts with the account:", deployer.address);
-  const balance = await deployer.getBalance();
-  console.log("Account balance:", ethers.utils.formatEther(balance), "ETH");
 
-  const SquaryFactory = await ethers.getContractFactory("Squary");
+  const SquaryFactory = await ethers.getContractFactory("SquaryV2");
   const squary = await SquaryFactory.deploy(usdcTokenAddress);
 
   await squary.deployed();
