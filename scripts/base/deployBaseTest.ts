5300d9d6070f5846dd168158e702129b8a25daf9
fabiandiaz416 <diazfabian99@icloud.com>
2024-11-26 12:39:28 -0300

    last changes after re deploy the contract to base testnet

--- scripts/base/deployBaseTest.ts
+++ scripts/base/deployBaseTest.ts
@@ -6,10 +6,10 @@ async function main() {
   const usdtTokenAddress = "0xd0602be1b9c3ED0715Be5786AD34114D9Da737BD";
   const daiTokenAddress = "0x75830602E8DE3048C12a73630B6d03cE74ACc643";
 
-  //  Get the signer (wallet) that is deploying the contract
+  // Get the signer (wallet) that is deploying the contract
   const [deployer] = await ethers.getSigners();
 
-  //  Print the deployer address and his balance
+  // Print the deployer address and his balance
   console.log("Deploying contracts with the account:", deployer);
 
   const SquaryFactory = await ethers.getContractFactory("SquaryBaseTest", deployer);
